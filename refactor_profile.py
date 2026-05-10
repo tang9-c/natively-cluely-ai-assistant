613c29a62b2ce81738a37602f02fa85229f69b61
evinjohnn <evinjohnignatious@gmail.com>
2026-05-11 03:15:55 +0530

    chore: remove specified files and directories from tracking

--- refactor_profile.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('src/components/ProfileIntelligenceSettings.tsx', 'r') as f:
-    content = f.read()
-
-# I will replace the entire ProfileIntelligenceSettings.tsx with a new version
-# that preserves all the Electron API calls, state logic, and renders an Asymmetric Bento Grid.
-
-# I don't want to copy all 1000 lines into python, so I will just write a new file using write_to_file from the tool.
