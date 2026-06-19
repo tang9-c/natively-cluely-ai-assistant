012904f1ebf017e19d1ba64f7d375853df48d6bd
tang9-c <tangdu@gmail.com>
2026-06-18 23:57:19 -0700

    chore: untrack dist-electron-test-isolated build artifacts
    These compiled .js/.js.map files were accidentally committed alongside
    the source refactor in ab56b91. Move the directory to .gitignore and
    remove from tracking to keep future diffs focused on source changes.

--- dist-electron-test-isolated/electron/services/profile/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DocType = void 0;
-var DocType;
-(function (DocType) {
-    DocType["RESUME"] = "resume";
-    DocType["JD"] = "job_description";
-})(DocType || (exports.DocType = DocType = {}));
-//# sourceMappingURL=types.js.map
--- dist-electron-test-isolated/electron/services/screen/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// electron/services/screen/types.ts
-// Shared types for the screen understanding layer.
-// Extracted from PromptAssembler.ts to provide a single canonical definition.
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
